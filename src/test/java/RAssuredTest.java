3f7d215362c58adedb298663cca4a9443e16d41d
katesem <katewhite922@gmail.com>
2022-04-21 21:58:18 +0300

    maven project

---
+++ src/test/java/RAssuredTest.java
@@ -0,0 +1,13 @@
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import org.testng.annotations.Test;
+
+public class RAssuredTest {
+
+    @Test
+    public void exampleOfTestNgMaven() {
+        System.out.println("This is TestNG-Maven Example");
+    }
+
+
+}
